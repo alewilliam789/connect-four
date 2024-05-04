fc09f4d51732ba494fa54b9ee0d0d96b7fb09212
Alex <alewilliam789@gmail.com>
2024-05-04 09:37:40 -0700

    Add the ability for the computer to make moves & change player state

--- src/app/features/game-board/game-board.component.ts
+++ src/app/features/game-board/game-board.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnDestroy, OnInit, Renderer2 } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { GameSessionService } from '../../core/services/game-session.service';
 import { numSequence } from '../../shared/utils'
-import { Winner } from 'src/app/shared/types';
-import { ComputerService } from 'src/app/core/services/computer.service';
+import { Winner } from '../../shared/types';
+import { ComputerService } from '../../core/services/computer.service';
 
 @Component({
   selector: 'app-game-board',
@@ -13,9 +13,11 @@ export class GameBoardComponent  implements OnInit {
   
   public numberArray = numSequence;
 
-  public currentPlayer : number = 1;
+  public currentPlayer : number = 0;
 
-  private winner : Winner = {player: 1, didWin: false};
+  private winner : Winner = {player: 0, didWin: false};
+
+  private previousMove : number = 0;
 
 
   constructor(private currentGame : GameSessionService, private renderer : Renderer2, private computer : ComputerService){}
@@ -29,6 +31,7 @@ export class GameBoardComponent  implements OnInit {
     this.currentGame.getResetState().subscribe((reset)=>{
       for(let i = 1; i <= 7; i++){
         document.getElementById(`sub-column-${i}`)?.replaceChildren();
+
       }
     })
 
@@ -37,7 +40,7 @@ export class GameBoardComponent  implements OnInit {
       if(this.currentPlayer != player){
         this.currentPlayer = player;
 
-        if(player == 2 && this.computer.isComputer){
+        if(player == 1 && this.computer.isComputer){
             this.computerMove();
             this.computer.isTurn = false;
         }
@@ -84,7 +87,7 @@ export class GameBoardComponent  implements OnInit {
 
  
     this.renderer.addClass(piece,'c-board__piece');
-    this.renderer.addClass(piece,`c-board__piece--player-${currentPlayer}`);
+    this.renderer.addClass(piece,`c-board__piece--player-${currentPlayer+1}`);
     this.renderer.addClass(highlight, 'c-board__highlight');
     this.renderer.addClass(highlight, 'c-board__piece--winner');
   
@@ -112,29 +115,33 @@ export class GameBoardComponent  implements OnInit {
 
 
   public playerMove(el : HTMLElement) {
+    let moveColumn = Number(el.id[el.id.length-1])-1;
+
     if(this.computer.isComputer){
-      if(!this.computer.isTurn && this.currentPlayer == 1){
-        this.addCounterToColumn(Number(el.id[el.id.length-1])-1,1);
+      if(!this.computer.isTurn && this.currentPlayer == 0){
+        this.previousMove = moveColumn;
+        this.addCounterToColumn(moveColumn,0);
         this.computer.isTurn = true;
-        this.currentGame.setCurrentPlayer(1);
+        this.currentGame.setCurrentPlayer(0);
       }
     }
     else {
-      this.addCounterToColumn(Number(el.id[el.id.length-1])-1,this.currentPlayer);
+      this.addCounterToColumn(moveColumn,this.currentPlayer);
       this.currentGame.setCurrentPlayer(this.currentPlayer);
     }
   }
 
+
+
   private computerMove() {
-    
-    let foundColumn = this.computer.makeMove();
 
     if(this.computer.isTurn){
       setTimeout(()=>{
-        while(!this.addCounterToColumn(foundColumn, 2)){
-          foundColumn = this.computer.makeMove();
+        let foundColumn = this.computer.makeMove(this.previousMove);
+        while(!this.addCounterToColumn(foundColumn, 1)&& !this.winner.didWin){
+          foundColumn = this.computer.makeMove(this.previousMove);
         }
-        this.currentGame.setCurrentPlayer(2);
+        this.currentGame.setCurrentPlayer(1);
       },2000)
       this.computer.isTurn = false;
     }
